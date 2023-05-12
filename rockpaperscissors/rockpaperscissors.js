6a443ad7ca45f88af96e60fbe2ef27866f39c741
clappedidiot <115187240+clappedidiot@users.noreply.github.com>
2023-05-12 19:04:18 +0100

    Removed comment line

--- rockpaperscissors/rockpaperscissors.js
+++ rockpaperscissors/rockpaperscissors.js
@@ -1,61 +1,55 @@
-// define Scores
-
-
-//define get Computer Choice function
-
-
-
-// get player choice using prompt
-
-
-//get computer choice
-
-
-// define a function that takes the two choices as arguments
-
 let playGame = (numberOfGames) => {
   let playerScore = 0;
   let computerScore = 0;
 
-  for ( let i = 0; i < numberOfGames; i++ ) {
-
+  for (let i = 0; i < numberOfGames; i++) {
     let getComputerChoice = () => {
       let choices = ["rock", "paper", "scissors"];
       return choices[Math.floor(Math.random() * 3)];
     };
     let computerChoice = getComputerChoice();
 
-    let playerChoice = prompt("Choose your weapon","Rock, Paper or Scissors?").toLowerCase();
+    let playerChoice = prompt(
+      "Choose your weapon",
+      "Rock, Paper or Scissors?"
+    ).toLowerCase();
 
     if (playerChoice === computerChoice) {
-      console.log("Tie")
+      console.log("Tie");
       playerScore = playerScore;
     } else if (
       (playerChoice === "rock" && computerChoice === "scissors") ||
       (playerChoice === "paper" && computerChoice === "rock") ||
       (playerChoice === "scissors" && computerChoice === "paper")
     ) {
-      console.log("Win")
+      console.log("Win");
       playerScore++;
     } else if (
       (playerChoice === "rock" && computerChoice === "paper") ||
       (playerChoice === "paper" && computerChoice === "scissors") ||
       (playerChoice === "scissors" && computerChoice === "rock")
     ) {
-      console.log("Lose")
+      console.log("Lose");
       computerScore++;
     }
   }
 
-  if ( playerScore > computerScore ) {
-    console.log(`The Score was ` + `${playerScore}` + ` : ` + `${computerScore} You won!`)
-  } else if ( playerScore < computerScore ) {
-    console.log(`The Score was ` + `${playerScore}` + ` : ` + `${computerScore} You lost!`)
+  if (playerScore > computerScore) {
+    console.log(
+      `The Score was ` + `${playerScore}` + ` : ` + `${computerScore} You won!`
+    );
+  } else if (playerScore < computerScore) {
+    console.log(
+      `The Score was ` + `${playerScore}` + ` : ` + `${computerScore} You lost!`
+    );
   } else {
-    console.log(`The Score was ` + `${playerScore}` + ` : ` + `${computerScore} So it was a Tie!`)
+    console.log(
+      `The Score was ` +
+        `${playerScore}` +
+        ` : ` +
+        `${computerScore} So it was a Tie!`
+    );
   }
-  
 };
 
 playGame(5);
-
