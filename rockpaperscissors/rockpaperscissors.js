1e980e418a9f805753416df0efbd8c1a3a81740c
clappedidiot <115187240+clappedidiot@users.noreply.github.com>
2023-05-15 21:58:57 +0100

    Create RPS App

--- rockpaperscissors/rockpaperscissors.js
+++ rockpaperscissors/rockpaperscissors.js
@@ -1,40 +1,62 @@
-let getComputerChoice = () => {
-  let choices = ["rock", "paper", "scissors"];
-  return choices[Math.floor(Math.random() * 3)];
-};
-
-let playerChoice = prompt(
-  "Choose your weapon",
-  "Rock, Paper or Scissors?"
-).toLowerCase();
-let computerChoice = "rock";
-
+    // set scores
 let playerScore = 0;
 let computerScore = 0;
 
-let playGame = (playerChoice, computerChoice) => {
+let playGame = (playerChoice) => {
+ 
+    // get computer choice randomly
+  let getComputerChoice = () => {
+    let choices = ["rock", "paper", "scissors"];
+    return choices[Math.floor(Math.random() * 3)];
+  };
+  let computerChoice = getComputerChoice();
+
+    // possible outcomes
   if (playerChoice === computerChoice) {
+    document.getElementById("result-box").textContent = "Tie";
     playerScore = playerScore;
   } else if (
     (playerChoice === "rock" && computerChoice === "scissors") ||
     (playerChoice === "paper" && computerChoice === "rock") ||
     (playerChoice === "scissors" && computerChoice === "paper")
   ) {
+    document.getElementById("result-box").textContent = "Win";
     playerScore++;
   } else if (
     (playerChoice === "rock" && computerChoice === "paper") ||
     (playerChoice === "paper" && computerChoice === "scissors") ||
     (playerChoice === "scissors" && computerChoice === "rock")
   ) {
+    document.getElementById("result-box").textContent = "Lose";
     computerScore++;
   }
-}
+    updateScores();
 
-let game = () => {
-    playGame();
-    playGame();
-    playGame();
-    playGame();
-    playGame();
-    return console.log(playerScore,computerScore)
+    // define winner
+    if (playerScore == 5) {
+        document.getElementById("winner").textContent = "You Win!"
+    } else if (computerScore ==5) {
+        document.getElementById("winner").textContent = "You Lose!"
+    }
+
+    
 };
+    
+    let rock = document.getElementById('rock');
+    let paper = document.getElementById('paper');
+    let scissors = document.getElementById('scissors');
+
+    rock.addEventListener('click', function(){
+        playGame('rock')
+    });
+    paper.addEventListener('click', function(){
+        playGame('paper')
+    });
+    scissors.addEventListener('click', function(){
+        playGame('scissors')
+    });
+
+    let updateScores = () => {
+        document.getElementById("player-score").textContent = `Player Score: ${playerScore}`;
+        document.getElementById("pc-score").textContent = `Computer Score: ${computerScore}`;
+    };
