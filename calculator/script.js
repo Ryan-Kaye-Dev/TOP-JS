9f10fcddfb9bed93c92d0e031eed9bc0a5dd4e65
clappedidiot <115187240+clappedidiot@users.noreply.github.com>
2023-05-29 22:20:41 +0100

    Fixed issues with multiple operators and chaining numbers

--- calculator/script.js
+++ calculator/script.js
@@ -1,74 +1,113 @@
-   // define numbers and operator
-   const firstNumber = 0;
-   const secondNumber = 0;
-   const operators = ['+','-','*','/'];
+// define numbers and operator
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = '';
+let op = false;
+//define variable for current display
+let currentDisplay = "";
+let secondDisplay = "";
 
-   // create mathematical functions
-const add = (a,b) => {
-    return a + b
+// create mathematical functions
+const add = (a, b) => {
+  return a + b;
 };
 
-const substract = (a,b) => {
-    return a - b
+const substract = (a, b) => {
+  return a - b;
 };
 
-const multiply = (a,b) => {
-    return a * b
+const multiply = (a, b) => {
+  return a * b;
 };
 
-const divide = (a,b) => {
-    return a / b
+const divide = (a, b) => {
+  return a / b;
 };
 
-
-   // define operate function
+// define operate function
 let operate = (firstNumber, operator, secondNumber) => {
-    if (operator === '+') {
-        return add(firstNumber, secondNumber)
-    } else if (operator === '-') {
-        return substract(firstNumber, secondNumber)
-    } else if (operator === '*') {
-        return multiply(firstNumber, secondNumber)
-    } else if (operator === '/') {
-        return divide(firstNumber, secondNumber)
+  if (operator === "+") {
+    return add(firstNumber, secondNumber);
+  } else if (operator === "-") {
+    return substract(firstNumber, secondNumber);
+  } else if (operator === "*") {
+    return multiply(firstNumber, secondNumber);
+  } else if (operator === "÷") {
+    return divide(firstNumber, secondNumber);
+  }
+};
+
+//define buttons
+let numButtons = Array.from(document.getElementsByClassName("num-button"));
+let opButtons = Array.from(document.getElementsByClassName("op-button"));
+let eqButton = Array.from(document.getElementsByClassName("eq-button"));
+let clrButton = Array.from(document.getElementsByClassName("clr-button"));
+let delButton = Array.from(document.getElementsByClassName("del-button"));
+
+//define updateDisplays
+let updateDisplay = (text) => {
+  document.getElementById("display-current").textContent = text;
+}
+
+let updateDisplay2 = (text) => {
+  document.getElementById("display-prev").textContent = text;
+}
+
+//add event listener for num buttons
+numButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    let clickedButton = this.innerText;
+    currentDisplay += clickedButton;
+    updateDisplay(currentDisplay);
+  });
+});
+
+// add event listener for operator buttons
+opButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    if (currentDisplay !== "") {
+      if (firstNumber === 0) {
+        // Store the first number if it has not been set yet
+        firstNumber = parseFloat(currentDisplay);
+      } else {
+        // Perform calculation on the fly if the first number and operator are already set
+        secondNumber = parseFloat(currentDisplay);
+        firstNumber = operate(firstNumber, operator, secondNumber);
+        updateDisplay(firstNumber);
+      }
+      operator = this.innerText;
+      updateDisplay2(operator)
+      currentDisplay = "";
     }
-   };
+  });
+});
 
-   //define variable for current display
-   let currentDisplay = '';
-   
-   //define buttons
-   let numButtons = Array.from(document.getElementsByClassName('num-button'));
-   let opButtons = Array.from(document.getElementsByClassName('op-button'));
-   let eqButton = Array.from(document.getElementsByClassName('eq-button'));
+//add event listener for equals button
+eqButton.forEach((button) => {
+  button.addEventListener("click", function() {
+    secondNumber = parseFloat(currentDisplay);
+    let answer = operate(firstNumber, operator, secondNumber);
+    updateDisplay(answer);
+  })
+})
 
+//add event listener for clr button
+clrButton.forEach((button) => {
+    button.addEventListener("click", function () {
+        currentDisplay = '';
+        document.getElementById("display-current").textContent = '';
+    });
+  });
 
-   //add event listener for num buttons
-   numButtons.forEach((button) => {
-    button.addEventListener('click', function(){
-        const clickedButton = this.innerText;
-        currentDisplay += clickedButton
-        document.getElementById('display-current').textContent = currentDisplay
-    })
-   });
-
-    //add event listener for operator buttons
-    opButtons.forEach((button) => {
-        button.addEventListener('click', function(){
-            let firstNumber = currentDisplay;
-            currentDisplay = '';
-            const operator = this.innerText;
-            document.getElementById('display-prev').textContent = operator;
-            document.getElementById('display-current').textContent = currentDisplay
-        })
-       });
-    
-       //add event listener for equals buttons
-    eqButton.forEach((button) => {
-        button.addEventListener('click', function(){
-            const clickedButton = this.innerText;
-            currentDisplay += clickedButton
-            document.getElementById('display-current').textContent = currentDisplay
-        })
-       });
-   
+  //add event listener for del button
+delButton.forEach((button) => {
+    button.addEventListener("click", function () {
+        currentDisplay = '';
+        document.getElementById("display-current").textContent = '';
+        document.getElementById("display-prev").textContent = 'DEL';
+    });
+    firstNumber = 0;
+    secondNumber = 0;
+    operator = '';
+    currentDisplay = '';
+  });
